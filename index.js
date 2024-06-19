28d5b676d299061e39a6814ed32fe567918a6713
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-06-19 23:09:27 +0200

    first step done

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+console.log(process.platform);
+
+// global.foo = 3;
+// console.log(foo);
+
+// console.log(process.argv);
+
+// setImmediate(function () {
+//   console.log("setImmediate callback");
+// });
+
+// process.nextTick(function () {
+//   console.log("NextTick callback");
+// });
+
+// NextTick callback
+// setImmediate callback
+
+// const fs = require("fs");
+// const path = require("path");
---
+++ main.js
@@ -0,0 +1,4 @@
+const logger = require("./module");
+
+logger.info("info function");
+logger.log("log function");
